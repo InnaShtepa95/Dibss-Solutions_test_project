c55f561933c590aebe299cd9cde66b67047a1575
InnaShtepa95 <inna.shtepa@gmail.com>
2019-01-16 16:42:10 +0200

    delivery block

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
 
         responsive:{
             0:{
-                items:1
+                items:4
             },
             600:{
-                items:1
+                items:4
             },
             1000:{
-                items:1
+                items:4
             }
         }
     });
