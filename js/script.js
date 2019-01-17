e8041c89100417797c26862c57d9e4820905ef40
InnaShtepa95 <inna.shtepa@gmail.com>
2019-01-17 19:24:19 +0200

    add right quoters to carousel

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:0,
         nav:true,
-        navText: [ ' ', ' ' ],
+        navText: ['<i class="fa fa-angle-left fa-2x" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right fa-2x" aria-hidden="true"></i>'],
         responsive:{
             0:{
                 items:4
@@ -19,4 +20,29 @@ $(document).ready(function(){
     });
 });
 
+*/
+$(document).ready(function () {
+    {
+        $('.owl-carousel').owlCarousel({
+            margin: 0,
+            loop: true,
+            responsiveClass: true,
+            items: 1,
+            nav: true,
+            navText: ['<i class="fas fa-angle-double-left" aria-hidden="true"></i>',
+                '<i class="fas fa-angle-double-right" aria-hidden="true"></i>'],
+            responsive: {
+                0:{
+                    items:4
+                },
+                600:{
+                    items:4
+                },
+                1000:{
+                    items:4
+                }
+            }
+        });
+    }
+});
 
